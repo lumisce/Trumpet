a804f7cfb0406e668ffdb43f054fdd53780de85d
lumisce <sinehye@gmail.com>
2019-11-05 03:41:20 +0900

    Add Register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,16 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Login from './auth/Login'
+import Register from './auth/Register'
 
 class App extends Component {
-	state = {
-		response: '',
-		post: '',
-		responseToPost: '',
-	}
-	componentDidMount() {
-		this.callApi()
-			.then(res => this.setState({response: res.express}))
-			.catch(err => console.log(err))
-	}
-
-	callApi = async () => {
-		const response = await fetch('/api/hello')
-		const body = await response.json()
-		if (response.status !== 200) {
-			throw Error(body.message)
-		}
-		return body
-	}
-
-	handleSubmit = async e => {
-		e.preventDefault()
-		const response = await fetch('/api/world', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({post: this.state.post}),
-		})
-		const body = await response.text()
-		this.setState({responseToPost: body})
-	}
-
 	render() {
 		return (
-			<div className="App">
-				<p>{this.state.response}</p>
-				<form onSubmit={this.handleSubmit}>
-					<p>
-						<strong>Post to Server</strong>
-					</p>
-					<input type="text" value={this.state.post} onChange={e => this.setState({post:e.target.value})} />
-					<button type="submit">Submit</button>
-				</form>
-				<p>{this.state.responseToPost}</p>
+			<div className="container mx-auto flex justify-center min-h-screen">
+				<Switch>
+					<Route path="/register" component={Register}/>
+					<Route path="/login" component={Login}/>
+				</Switch>
 			</div>
 		)
 	}
---
+++ client/src/auth/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+	render() {
+		return (
+			<div className="m-6">
+				<h1 className="block text-teal-600 text-center text-2xl font-bold mb-4">Login</h1>
+			</div>
+		)
+	}
+}
+
+export default Login
